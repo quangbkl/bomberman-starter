f9fef4acd3e4658f2bb116526f15c1efc1f116b9
Quang BKL <quang99dev@gmai.com>
2018-10-31 18:50:30 +0700

    move bomber

--- src/uet/oop/bomberman/entities/character/Bomber.java
+++ src/uet/oop/bomberman/entities/character/Bomber.java
@@ -7,6 +7,7 @@ import uet.oop.bomberman.entities.bomb.Bomb;
 import uet.oop.bomberman.graphics.Screen;
 import uet.oop.bomberman.graphics.Sprite;
 import uet.oop.bomberman.input.Keyboard;
+import uet.oop.bomberman.level.Coordinates;
 
 import java.util.Iterator;
 import java.util.List;
@@ -68,15 +69,23 @@ public class Bomber extends Character {
      * Kiểm tra xem có đặt được bom hay không? nếu có thì đặt bom tại vị trí hiện tại của Bomber
      */
     private void detectPlaceBomb() {
-        // TODO: kiểm tra xem phím điều khiển đặt bom có được gõ và giá trị _timeBetweenPutBombs, Game.getBombRate() có thỏa mãn hay không
-        // TODO:  Game.getBombRate() sẽ trả về số lượng bom có thể đặt liên tiếp tại thời điểm hiện tại
-        // TODO: _timeBetweenPutBombs dùng để ngăn chặn Bomber đặt 2 Bomb cùng tại 1 vị trí trong 1 khoảng thời gian quá ngắn
-        // TODO: nếu 3 điều kiện trên thỏa mãn thì thực hiện đặt bom bằng placeBomb()
-        // TODO: sau khi đặt, nhớ giảm số lượng Bomb Rate và reset _timeBetweenPutBombs về 0
+        // @todo: kiểm tra xem phím điều khiển đặt bom có được gõ và giá trị _timeBetweenPutBombs, Game.getBombRate() có thỏa mãn hay không
+        // @todo:  Game.getBombRate() sẽ trả về số lượng bom có thể đặt liên tiếp tại thời điểm hiện tại
+        // @todo: _timeBetweenPutBombs dùng để ngăn chặn Bomber đặt 2 Bomb cùng tại 1 vị trí trong 1 khoảng thời gian quá ngắn
+        // @todo: nếu 3 điều kiện trên thỏa mãn thì thực hiện đặt bom bằng placeBomb()
+        // @todo: sau khi đặt, nhớ giảm số lượng Bomb Rate và reset _timeBetweenPutBombs về 0
+        int bombRate = Game.getBombRate();
+        if (_input.space && _timeBetweenPutBombs < 0 && bombRate > 1) {
+            placeBomb(Coordinates.pixelToTile(_x), Coordinates.pixelToTile(_y));
+            Game.addBombRate(-1);
+            _timeBetweenPutBombs = 0;
+        }
     }
 
     protected void placeBomb(int x, int y) {
-        // TODO: thực hiện tạo đối tượng bom, đặt vào vị trí (x, y)
+        // @todo thực hiện tạo đối tượng bom, đặt vào vị trí (x, y)
+        Bomb bomb = new Bomb(x, y, _board);
+        _bombs.add(bomb);
     }
 
     private void clearBombs() {
@@ -109,20 +118,40 @@ public class Bomber extends Character {
 
     @Override
     protected void calculateMove() {
-        // TODO: xử lý nhận tín hiệu điều khiển hướng đi từ _input và gọi move() để thực hiện di chuyển
-        // TODO: nhớ cập nhật lại giá trị cờ _moving khi thay đổi trạng thái di chuyển
+        // @todo: xử lý nhận tín hiệu điều khiển hướng đi từ _input và gọi move() để thực hiện di chuyển
+        // @todo: nhớ cập nhật lại giá trị cờ _moving khi thay đổi trạng thái di chuyển
+        _moving = true;
+        if (_input.up) {
+            move(_x, _y - 1);
+        } else if (_input.down) {
+            move(_x, _y + 1);
+        } else if (_input.left) {
+            move(_x - 1, _y);
+        } else if (_input.right) {
+            move(_x + 1, _y);
+        } else {
+            _moving = false;
+        }
     }
 
     @Override
     public boolean canMove(double x, double y) {
         // TODO: kiểm tra có đối tượng tại vị trí chuẩn bị di chuyển đến và có thể di chuyển tới đó hay không
-        return false;
+        return true;
     }
 
     @Override
     public void move(double xa, double ya) {
-        // TODO: sử dụng canMove() để kiểm tra xem có thể di chuyển tới điểm đã tính toán hay không và thực hiện thay đổi tọa độ _x, _y
-        // TODO: nhớ cập nhật giá trị _direction sau khi di chuyển
+        // @todo: sử dụng canMove() để kiểm tra xem có thể di chuyển tới điểm đã tính toán hay không và thực hiện thay đổi tọa độ _x, _y
+        // @todo: nhớ cập nhật giá trị _direction sau khi di chuyển : up, right, down, left -> 0, 1, 2, 3
+        if (canMove(xa, ya)) {
+            if (xa > _x) _direction = 1;
+            if (xa < _x) _direction = 3;
+            if (ya > _y) _direction = 2;
+            if (ya < _y) _direction = 0;
+            _x = xa;
+            _y = ya;
+        }
     }
 
     @Override
--- src/uet/oop/bomberman/level/FileLevelLoader.java
+++ src/uet/oop/bomberman/level/FileLevelLoader.java
@@ -5,8 +5,12 @@ import uet.oop.bomberman.Game;
 import uet.oop.bomberman.entities.LayeredEntity;
 import uet.oop.bomberman.entities.character.Bomber;
 import uet.oop.bomberman.entities.character.enemy.Balloon;
+import uet.oop.bomberman.entities.character.enemy.Oneal;
 import uet.oop.bomberman.entities.tile.Grass;
+import uet.oop.bomberman.entities.tile.Portal;
 import uet.oop.bomberman.entities.tile.destroyable.Brick;
+import uet.oop.bomberman.entities.tile.item.BombItem;
+import uet.oop.bomberman.entities.tile.item.FlameItem;
 import uet.oop.bomberman.entities.tile.item.SpeedItem;
 import uet.oop.bomberman.exceptions.LoadLevelException;
 import uet.oop.bomberman.graphics.Screen;
@@ -28,8 +32,8 @@ public class FileLevelLoader extends LevelLoader {
 
     @Override
     public void loadLevel(int level) {
-        // TODO: đọc dữ liệu từ tệp cấu hình /levels/Level{level}.txt
-        // TODO: cập nhật các giá trị đọc được vào _width, _height, _level, _map
+        // @todo: đọc dữ liệu từ tệp cấu hình /levels/Level{level}.txt
+        // @todo: cập nhật các giá trị đọc được vào _width, _height, _level, _map
         try {
             InputStream inputStream = new FileInputStream("res/levels/level" + level + ".txt");
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
@@ -63,11 +67,11 @@ public class FileLevelLoader extends LevelLoader {
 
     @Override
     public void createEntities() {
-        // TODO: tạo các Entity của màn chơi
-        // TODO: sau khi tạo xong, gọi _board.addEntity() để thêm Entity vào game
+        // @todo: tạo các Entity của màn chơi
+        // @todo: sau khi tạo xong, gọi _board.addEntity() để thêm Entity vào game
 
-        // TODO: phần code mẫu ở dưới để hướng dẫn cách thêm các loại Entity vào game
-        // TODO: hãy xóa nó khi hoàn thành chức năng load màn chơi từ tệp cấu hình
+        // @todo: phần code mẫu ở dưới để hướng dẫn cách thêm các loại Entity vào game
+        // @todo: hãy xóa nó khi hoàn thành chức năng load màn chơi từ tệp cấu hình
         for (int x = 0; x < _width; x++) {
             for (int y = 0; y < _height; y++) {
                 // thêm Wall
@@ -89,6 +93,11 @@ public class FileLevelLoader extends LevelLoader {
                     _board.addCharacter(new Balloon(Coordinates.tileToPixel(xE), Coordinates.tileToPixel(yE) + Game.TILES_SIZE, _board));
                     _board.addEntity(xE + yE * _width, new Grass(xE, yE, Sprite.grass));
                 }
+                if (_map[y][x] == '2') {
+                    int xE = x, yE = y;
+                    _board.addCharacter(new Oneal(Coordinates.tileToPixel(xE), Coordinates.tileToPixel(yE) + Game.TILES_SIZE, _board));
+                    _board.addEntity(xE + yE * _width, new Grass(xE, yE, Sprite.grass));
+                }
 
                 // thêm Brick
                 if (_map[y][x] == '*') {
@@ -102,6 +111,26 @@ public class FileLevelLoader extends LevelLoader {
                 }
 
                 // thêm Item kèm Brick che phủ ở trên
+                if (_map[y][x] == 'x') {
+                    int xI = x, yI = y;
+                    _board.addEntity(xI + yI * _width,
+                            new LayeredEntity(xI, yI,
+                                    new Grass(xI, yI, Sprite.grass),
+                                    new Portal(xI, yI, Sprite.powerup_flames),
+                                    new Brick(xI, yI, Sprite.brick)
+                            )
+                    );
+                }
+                if (_map[y][x] == 'b') {
+                    int xI = x, yI = y;
+                    _board.addEntity(xI + yI * _width,
+                            new LayeredEntity(xI, yI,
+                                    new Grass(xI, yI, Sprite.grass),
+                                    new BombItem(xI, yI, Sprite.powerup_flames),
+                                    new Brick(xI, yI, Sprite.brick)
+                            )
+                    );
+                }
                 if (_map[y][x] == 'f') {
                     int xI = x, yI = y;
                     _board.addEntity(xI + yI * _width,
@@ -112,6 +141,17 @@ public class FileLevelLoader extends LevelLoader {
                             )
                     );
                 }
+                if (_map[y][x] == 's') {
+                    int xI = x, yI = y;
+                    _board.addEntity(xI + yI * _width,
+                            new LayeredEntity(xI, yI,
+                                    new Grass(xI, yI, Sprite.grass),
+                                    new FlameItem(xI, yI, Sprite.powerup_flames),
+                                    new Brick(xI, yI, Sprite.brick)
+                            )
+                    );
+                }
+
             }
         }
 
